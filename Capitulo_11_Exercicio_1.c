ab6e74ad5b7e284993a46daaaab09ff4868c0fee
JeanMoutinho98 <142322746+JeanMoutinho98@users.noreply.github.com>
2023-11-14 21:04:11 -0300

    Add files via upload

---
+++ Capitulo_11_Exercicio_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>                                                   //Okok
+
+int main(){
+
+    printf("Tamanho em bytes de um int: %d byte(s)\n", sizeof(int));
+    printf("Tamanho em bytes de um char: %d byte(s)\n", sizeof(char));
+    printf("Tamanho em bytes de um float: %d byte(s)\n", sizeof(float));
+    printf("Tamanho em bytes de um double: %d byte(s)\n", sizeof(double));
+    printf("Tamanho em bytes de um long long int: %d byte(s)\n", sizeof(long long int));
+
+
+    return 0;
+}
